f6be9646c2946c13ba3e4d96002990ef737bd344
Sha <shahb@softcrylic.co.in>
2012-05-25 20:11:53 +0530

    Reverted Changes

--- src/test/java/com/softcrylic/testautomation/pages/HomePage.java
+++ src/test/java/com/softcrylic/testautomation/pages/HomePage.java
@@ -17,7 +17,7 @@ public class HomePage {
 
     public SearchResultPage searchFor(String location) {
         try {
-            WebElement searchField = driver.findElement(By.id("sed"));
+            WebElement searchField = driver.findElement(By.id("sted"));
             searchField.clear();
             searchField.sendKeys(location);
             searchField.submit();
