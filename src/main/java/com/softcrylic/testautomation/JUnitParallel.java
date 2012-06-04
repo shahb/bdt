5541fe616fd123bdb0eefb672fd47977aa947eb2
shahb <shahb@live.in>
2012-06-04 19:34:51 +0530

    Modified JunitParallel.Java

--- src/main/java/com/softcrylic/testautomation/JUnitParallel.java
+++ src/main/java/com/softcrylic/testautomation/JUnitParallel.java
@@ -1,17 +1,17 @@
 package com.softcrylic.testautomation;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Parameterized;
+//import org.junit.runners.Parameterized.Parameters;
+//import org.junit.runners.Suite;
+//import org.junit.runners.Suite.SuiteClasses;
 
 //import com.softcrylic.junit.Parallelized;
 
-import cucumber.junit.Cucumber;
+//import cucumber.junit.Cucumber;
 
 //@RunWith(Parallelized.class)
 //@Parallelized.MyClasses({AllTests.class,JunitClassTest.class})
-public class JUnitParallel {
+public class JUnitParallel{
 	
 }
