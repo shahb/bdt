a96d474c723db13cbc84cf57ae69929f29284a5e
shahb <shahb@live.in>
2012-06-05 22:20:54 +0530

    Changes

--- src/test/java/com/softcrylic/test/automation/RunNewYorkTests.java
+++ src/test/java/com/softcrylic/test/automation/RunNewYorkTests.java
@@ -4,7 +4,7 @@ import cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class) 
-@Cucumber.Options(features = "src\\test\\resources\\com\\softcrylic\\test\\automation\\newyork.feature",format = {"pretty", "html:target/newyork","json:target/newyork.json" })
+@Cucumber.Options(features = "src\\test\\resources\\com\\softcrylic\\test\\automation\\newyork.feature",format = {"pretty", "html:target/cucumber","json:target/newyork.json" })
 //@Cucumber.Options(format = {"pretty", "html:target/cucumber","json:target/cucumber.json" })
 public class RunNewYorkTests  {
 }
